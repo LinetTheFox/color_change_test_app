8cf96d7bec1f13ba219c6c62b273d775e06c5ec5
LinetTheFox <linetmoreandil@gmail.com>
2020-04-27 04:54:29 +0300

    Added extra feature - text colors is negative (i guess?) to the background

--- color_change_test_app/lib/widgets/random_color_widget.dart
+++ color_change_test_app/lib/widgets/random_color_widget.dart
@@ -12,18 +12,27 @@ class _RandomColorWidgetState extends State<RandomColorWidget> {
 
   Random _rng = new Random();
 
-  Color _currentColor = Colors.white;
+  Color _currentBgColor = Colors.white;
+  Color _currentTextColor = Colors.black;
 
   void _setRandomCurrentColor() {
+
     setState(() {
       // setting int bytes for random values of each RGB values separately.
       // (Alpha not generated and always at max)
       int alpha = 255 * 256 * 256 * 256;
-      int red = _rng.nextInt(255) * 256 * 256;
-      int green = _rng.nextInt(255) * 256;
-      int blue = _rng.nextInt(255);
+      int redBg = _rng.nextInt(255) * 256 * 256;
+      int greenBg = _rng.nextInt(255) * 256;
+      int blueBg = _rng.nextInt(255);
 
-      _currentColor = Color(alpha+red+green+blue);
+
+      // setting the opposite values to color of the text
+      int redText = 255 * 256 * 256 - redBg;
+      int greenText = 255 * 256 - greenBg;
+      int blueText = 255 - blueBg;
+
+      _currentBgColor = Color(alpha+redBg+greenBg+blueBg);
+      _currentTextColor = Color(alpha+redText+greenText+blueText);
     });
   }
 
@@ -32,13 +41,14 @@ class _RandomColorWidgetState extends State<RandomColorWidget> {
     return GestureDetector(
       onTap: () => _setRandomCurrentColor(),
       child: Container(
-        color: _currentColor,
+        color: _currentBgColor,
         child: Center(
           child: Text(
             "Hey there!",
             style: TextStyle(
               fontSize: 24,
               fontWeight: FontWeight.bold,
+              color: _currentTextColor,
             ),
           ),
         ),
